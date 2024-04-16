cc58fd7bb7b89a7515e9f22ead875efac369cf14
MORAIS FARIA joaquim (Externe) <joaquim.morais-faria-ext@ima.eu>
2024-04-16 14:03:13 +0200

    Fonction deposit = input player

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
 def deposit():
     while True:
-            
+        amount = input("Enter the amount to deposit : €")
+        if amount.isdigit():
+            amount = int(amount)
+            if amount > 0:
+                break
+            else:
+                print("Please enter a positive number")
+        else:
+            print("Please enter a positive number")
+                
+        return amount
+deposit()
